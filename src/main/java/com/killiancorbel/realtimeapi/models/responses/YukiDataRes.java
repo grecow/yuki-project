b8ca70d7d4f0afcbd99a003ae6588da422eb45d6
thekten <thekten@gmail.com>
2025-01-13 17:08:56 +0100

    trying to debug tokens

--- src/main/java/com/killiancorbel/realtimeapi/models/responses/YukiDataRes.java
+++ src/main/java/com/killiancorbel/realtimeapi/models/responses/YukiDataRes.java
@@ -11,6 +11,11 @@ public class YukiDataRes {
         this.prompt = getPromptFromModel(yukiData);
     }
 
+    public YukiDataRes(int tokens) {
+        this.tokens = tokens;
+        this.prompt = "";
+    }
+
     public int getTokens() {
         return tokens;
     }

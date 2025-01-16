30a7db29939e347655d16a4801344f367b54e6e1
thekten <thekten@gmail.com>
2025-01-16 17:52:01 +0100

    debuging

--- src/main/java/com/killiancorbel/realtimeapi/controllers/UserController.java
+++ src/main/java/com/killiancorbel/realtimeapi/controllers/UserController.java
@@ -5,12 +5,10 @@ import com.killiancorbel.realtimeapi.models.requests.LoginReq;
 import com.killiancorbel.realtimeapi.models.requests.RegisterReq;
 import com.killiancorbel.realtimeapi.models.responses.LoginRes;
 import com.killiancorbel.realtimeapi.repositories.UserRepository;
-import com.killiancorbel.realtimeapi.utils.JwtUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.AccessDeniedException;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -22,25 +20,18 @@ import org.springframework.web.server.ServerErrorException;
 public class UserController {
     @Autowired
     private UserRepository userRepository;
-    private final AuthenticationManager authenticationManager;
     private final Logger logger = LoggerFactory.getLogger(UserController.class);
 
-    private JwtUtil jwtUtil;
-    public UserController(AuthenticationManager authenticationManager, JwtUtil jwtUtil) {
-        this.authenticationManager = authenticationManager;
-        this.jwtUtil = jwtUtil;
+    public UserController() {
     }
 
     @PostMapping(path = "/login")
     public @ResponseBody LoginRes login(@RequestBody LoginReq inputUser) {
         try {
-            Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(inputUser.getEmail(), inputUser.getPassword()));
-            String email = authentication.getName();
-            User user = userRepository.findByEmail(email);
-            String token = jwtUtil.createToken(user);
+            User user = userRepository.findByEmail("");
             LoginRes res = new LoginRes();
             res.setEmail(user.getEmail());
-            res.setToken(token);
+            res.setToken("token");
             return res;
         }catch (BadCredentialsException e){
             throw new BadCredentialsException(e.getMessage(), e.getCause());
